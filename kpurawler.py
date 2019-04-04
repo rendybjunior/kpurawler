40b06c5355388a963ef563740520593439438193
Rendy B. Junior <rendy.b.junior@gmail.com>
2019-04-04 21:23:56 +0700

    mvp download all caleg cv for certain dapil

--- kpurawler.py
+++ kpurawler.py
@@ -1,12 +1,33 @@
-import scrapy
+import sys
+import time
 
-class BlogSpider(scrapy.Spider):
-    name = 'blogspider'
-    start_urls = ['https://blog.scrapinghub.com']
+import scrapy
+import json
+
+class DctSpider(scrapy.Spider):
+    name = 'dct_crawler'
+    dapil_id = '4782' # ID Dapil Cianjur 2
+    current_ts = round(time.time() * 1000)
+    start_urls = ['https://infopemilu.kpu.go.id/pileg2019/pencalonan/pengajuan-calon/' + \
+        str(dapil_id) + '/calonDct.json?_=' + str(current_ts)]
 
     def parse(self, response):
-        for title in response.css('.post-header>h2'):
-            yield {'title': title.css('a ::text').get()}
+        rows = json.loads(response.text)
+        for row in rows:
+            calon_id = row.get('id')
+            next_page = "https://silonpemilu.kpu.go.id/publik/calon/" + \
+                str(calon_id) + "/2" # 2 denotes 3rd file we download: track record
+            yield scrapy.Request(next_page, callback=self.save_pdf)
 
-        for next_page in response.css('a.next-posts-link'):
-            yield response.follow(next_page, self.parse)
+    def save_pdf(self, response):
+        path = response.url.split('/')[-2] + ".pdf"
+        if response.status == 200:
+            download_size = len(response.body)
+            if download_size == 0:
+                self.logger.warn('Zero kb response on %s', path)
+            else:
+                self.logger.info('Saving PDF %s', path)
+                with open(path, 'wb') as f:
+                    f.write(response.body)
+        else:
+            self.logger.warn('Not success response %s', response.status)
